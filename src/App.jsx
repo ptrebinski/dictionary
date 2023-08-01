404076b1f91e2d40d92e0e86f377d905fffd361d
Patryk Trębiński <trebinski.patryk@gmail.com>
2023-08-01 11:18:08 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white font-sans text-zinc-800">
+      <div className="mx-auto w-11/12 max-w-3xl">
+        <h1 className=" text-3xl font-bold ">Dictionary</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+/*eslint-env node*/
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#a445ed",
+        error: "#ff5252",
+      },
+    },
+  },
+  plugins: [],
+};
